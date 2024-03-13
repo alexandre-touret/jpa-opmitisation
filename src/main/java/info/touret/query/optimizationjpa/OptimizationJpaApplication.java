167157a81841f5b304cdcdb423776a2c8a931e17
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2024-03-13 21:20:15 +0100

    chore: Initialization

---
+++ src/main/java/info/touret/query/optimizationjpa/OptimizationJpaApplication.java
@@ -0,0 +1,14 @@
+package info.touret.query.optimizationjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizationJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizationJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/touret/query/optimizationjpa/OptimizationJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package info.touret.query.optimizationjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizationJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
